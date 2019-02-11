54ab0d8a94f9b67375e1c9798bc0e7df43a9c4d1
Greg Landrum <greg.landrum@gmail.com>
2019-02-11 13:16:06 +0100

    include setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='structurepipeline',
+      version='0.0.1',
+      description='ChEMBL Structure Pipeline',
+      url='https://www.ebi.ac.uk/chembl/',
+      author='Greg Landrum',
+      author_email='greg.landrum@t5informatics.com',
+      license='MIT',
+      packages=['structurepipeline'],
+      zip_safe=False)
