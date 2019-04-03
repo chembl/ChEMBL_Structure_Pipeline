611dafd03953ac0b0b040abc6f5bda6a1836a767
Greg Landrum <greg.landrum@gmail.com>
2019-04-03 18:20:23 +0200

    fix a typo and do some pep8 formatting

--- exclude_flag.py
+++ exclude_flag.py
@@ -5,9 +5,10 @@ mols_to_keep = ['exclude_mols/ranitidine_to_keep.mol', 'exclude_mols/CO-ADD1.mol
                 'exclude_mols/CO-ADD2.mol', 'exclude_mols/CO-ADD3.mol', 'exclude_mols/CO-ADD4.mol']
 
 # structures with vanadium that we want to keep
-vanadium_to_keep = ['exclude_mols/v1.mol', 'exclude_mols/v2.mol', 'exclude_mols/v3.mol', 'exclude_mols/v4.mol']
+vanadium_to_keep = ['exclude_mols/v1.mol', 'exclude_mols/v2.mol',
+                    'exclude_mols/v3.mol', 'exclude_mols/v4.mol']
 
-# Zn not in list mimicking old PP protocol 
+# Zn not in list mimicking old PP protocol
 METAL_LIST = ['Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Ga', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd',
               'Cd', 'In', 'Sn', 'La', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'Ac',
               'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Th', 'Pa', 'U', 'Np',
@@ -31,15 +32,17 @@ def exclude_flag(molfile):
     except:
         rdkit_fails = True
 
-    #########  exclude all molecules that can't be sanitized by RDKit except the ones in the two weird sets
+    # exclude all molecules that can't be sanitized by RDKit except the ones in the two weird sets
 
     # molecules that won't be excluded, mols_to_keep
-    matches = [mol.HasSubstructMatch(Chem.MolFromMolFile(x, sanitize=False)) for x in mols_to_keep]
+    matches = [mol.HasSubstructMatch(Chem.MolFromMolFile(
+        x, sanitize=False)) for x in mols_to_keep]
     if True in matches:
         return exclude
 
     # structures with vanadium to keep
-    matches = [mol.HasSubstructMatch(Chem.MolFromMolFile(x, sanitize=False)) for x in vanadium_to_keep]
+    matches = [mol.HasSubstructMatch(Chem.MolFromMolFile(
+        x, sanitize=False)) for x in vanadium_to_keep]
     # if any saved vanadium containing molecule matches with NotV, will be also excluded
     if True in matches and not mol.HasSubstructMatch(Chem.MolFromMolFile('exclude_mols/NotV.mol', sanitize=False)):
         return exclude
@@ -61,6 +64,4 @@ def exclude_flag(molfile):
     if (metallic and not (c_count == 0 and ok_count != 0)) or (not metallic and boron_count > 7) or rdkit_fails:
         exclude = True
 
-return exclude
-
-
+    return exclude
