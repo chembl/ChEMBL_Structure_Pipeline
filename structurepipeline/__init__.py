0ba4395b6cf15d12ca4f3e832a8d8f2d3bb1e63e
Greg Landrum <greg.landrum@gmail.com>
2019-07-12 10:29:22 +0200

    expand public API and add doctests for it

--- structurepipeline/__init__.py
+++ structurepipeline/__init__.py
@@ -40,8 +40,66 @@ was found:
     ()
     >>>
 
+# Standardizing structures:
+
+    >>> mb = standardize_molblock('''
+    ...   Mrv1810 07121910172D          
+    ... 
+    ...   4  3  0  0  0  0            999 V2000
+    ...    -2.5038    0.4060    0.0000 C   0  0  3  0  0  0  0  0  0  0  0  0
+    ...    -2.5038    1.2310    0.0000 O   0  5  0  0  0  0  0  0  0  0  0  0
+    ...    -3.2182   -0.0065    0.0000 N   0  3  0  0  0  0  0  0  0  0  0  0
+    ...    -1.7893   -0.0065    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    ...   1  2  1  0  0  0  0
+    ...   1  3  1  0  0  0  0
+    ...   1  4  1  4  0  0  0
+    ... M  CHG  2   2  -1   3   1
+    ... M  END
+    ... ''')
+    >>> print(mb)
+    <BLANKLINE>
+         RDKit          2D
+    <BLANKLINE>
+      4  3  0  0  0  0  0  0  0  0999 V2000
+       -2.5038    0.4060    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+       -2.5038    1.2310    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0
+       -3.2182   -0.0065    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0
+       -1.7893   -0.0065    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+      1  2  1  0
+      1  3  1  0
+      1  4  1  0
+    M  END
+    <BLANKLINE>
+   >>>
+
+# Standardization + salt stripping :
+    >>> mb = get_parent_molblock('''
+    ...   Mrv1810 07121910262D          
+    ... 
+    ...   3  1  0  0  0  0            999 V2000
+    ...    -5.2331    1.1053    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    ...    -4.5186    1.5178    0.0000 N   0  3  0  0  0  0  0  0  0  0  0  0
+    ...    -2.8647    1.5789    0.0000 Cl  0  5  0  0  0  0  0  0  0  0  0  0
+    ...   1  2  1  0  0  0  0
+    ... M  CHG  2   2   1   3  -1
+    ... M  END
+    ... ''')
+    >>> print(mb)
+    <BLANKLINE>
+         RDKit          2D
+    <BLANKLINE>
+      2  1  0  0  0  0  0  0  0  0999 V2000
+       -5.2331    1.1053    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+       -4.5186    1.5178    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0
+      1  2  1  0
+    M  END
+    <BLANKLINE>
+    >>>
+
 """
 from .checker import check_molblock
+from .standardizer import standardize_molblock, standardize_mol
+from .standardizer import get_parent_molblock, get_parent_mol
 __version__ = "0.0.1"
 
 #
