6c0e17a9014f3701220be6b5f769f5dbaa459123
Eloy Felix <eloyfelix@gmail.com>
2019-08-09 14:38:51 +0100

    update exclude_flag function

--- structurepipeline/exclude_flag.py
+++ structurepipeline/exclude_flag.py
@@ -1,30 +1,21 @@
 from rdkit import Chem
-import os
-
-_data_dir = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "data")
-
-# list of mols to keep no matter what
-mols_to_keep = ['exclude_mols/ranitidine_to_keep.mol', 'exclude_mols/CO-ADD1.mol',
-                'exclude_mols/CO-ADD2.mol', 'exclude_mols/CO-ADD3.mol', 'exclude_mols/CO-ADD4.mol']
-
-# structures with vanadium that we want to keep
-vanadium_to_keep = ['exclude_mols/v1.mol', 'exclude_mols/v2.mol',
-                    'exclude_mols/v3.mol', 'exclude_mols/v4.mol']
-
-# Zn not in list mimicking old PP protocol
-METAL_LIST = ['Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Ga', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd',
-              'Cd', 'In', 'Sn', 'La', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'Ac',
-              'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Th', 'Pa', 'U', 'Np',
-              'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Ge', 'Sb']
 
 
-def exclude_flag(molfile, dataDir='', includeRDKitSanitization=True):
+# Zn not in the list as we have some compounds in ChEMBL
+# most of them are simple salts
+METAL_LIST = [
+    "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Ga", "Y", "Zr", "Nb", "Mo",
+    "Tc", "Ru", "Rh", "Pd", "Cd", "In", "Sn", "La", "Hf", "Ta", "W", "Re", "Os", "Ir",
+    "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "Ac", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", 
+    "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Th", "Pa", "U", "Np", "Pu", "Am",
+    "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr","Ge", "Sb",
+]
+
+
+def exclude_flag(molfile, includeRDKitSanitization=True):
     """
-    Rules to exclude structures. All exceptions are because of the old PP protocol.
+    Rules to exclude structures. Ported and updated from original PP protocol.
     """
-    if not dataDir:
-        dataDir = _data_dir
     rdkit_fails = False
     exclude = False
     metallic = False
@@ -41,36 +32,27 @@ def exclude_flag(molfile, dataDir='', includeRDKitSanitization=True):
                 rdkit_fails = True
     else:
         mol = molfile
-    # exclude all molecules that can't be sanitized by RDKit except the ones in the two weird sets
-
-    # molecules that won't be excluded, mols_to_keep
-    matches = [mol.HasSubstructMatch(Chem.MolFromMolFile(
-        os.path.join(dataDir, x), sanitize=False)) for x in mols_to_keep]
-    if True in matches:
-        return exclude
-
-    # structures with vanadium to keep
-    matches = [mol.HasSubstructMatch(Chem.MolFromMolFile(
-        os.path.join(dataDir, x), sanitize=False)) for x in vanadium_to_keep]
-    # if any saved vanadium containing molecule matches with NotV, will be also excluded
-    if True in matches and not mol.HasSubstructMatch(Chem.MolFromMolFile(os.path.join(dataDir, 'exclude_mols/NotV.mol'), sanitize=False)):
-        return exclude
 
     for atom in mol.GetAtoms():
         a_type = atom.GetSymbol()
         if a_type in METAL_LIST:
             metallic = True
-        if a_type == 'B':
+        if a_type == "B":
             boron_count += 1
-        if a_type in ['Cu', 'Fe', 'Mn', 'Ni']:
+        if a_type in ["Cu", "Fe", "Mn", "Ni"]:
             ok_count += 1
-        if a_type == 'C':
+        if a_type == "C":
             c_count += 1
 
     # metallic or non metallic with more than 7 boron atoms will be excluded
-    # metallic molecules with 0 C atoms and cu,fe,mn, or ni won't be excluded
-    # not RDKit sanitizable will be excluded
-    if (metallic and not (c_count == 0 and ok_count != 0)) or (not metallic and boron_count > 7) or rdkit_fails:
+    #  - problem of depicting borane compounds
+    # RDKit non sanitizable molecules will also be excluded
+    # metallic molecules with no C atoms and Cu, Fe, Mn, or Ni won't be excluded
+    if (
+        (metallic and not (c_count == 0 and ok_count != 0))
+        or (not metallic and boron_count > 7)
+        or rdkit_fails
+    ):
         exclude = True
 
     return exclude
