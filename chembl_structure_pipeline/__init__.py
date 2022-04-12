11f8724c23d6dca151bd6da409d31ea72cd602f6
Dan Mason <dan.mason@healx.io>
2022-04-12 21:11:29 +0100

    add version back into __init__.py - need to update 2 files in future

--- chembl_structure_pipeline/__init__.py
+++ chembl_structure_pipeline/__init__.py
@@ -100,6 +100,7 @@ was found:
 from chembl_structure_pipeline.checker import check_molblock
 from chembl_structure_pipeline.standardizer import standardize_molblock, standardize_mol
 from chembl_structure_pipeline.standardizer import get_parent_molblock, get_parent_mol
+__version__ = "1.0.0"
 
 #
 #  Copyright (c) 2019 Greg Landrum
