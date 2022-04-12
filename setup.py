4767e7e72403623d9bece0de75e7bbe3b0e913e0
Dan Mason <dan.mason@healx.io>
2022-04-12 20:04:17 +0100

    remove circular import that upsets setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
-import chembl_structure_pipeline
 
-setup(name='chembl_structure_pipeline',
-      version=chembl_structure_pipeline.__version__,
-      description='ChEMBL Structure Pipeline',
-      url='https://www.ebi.ac.uk/chembl/',
-      author='Greg Landrum',
-      author_email='greg.landrum@t5informatics.com',
-      license='MIT',
-      packages=['chembl_structure_pipeline'],
-      package_data={'chembl_structure_pipeline': ['data/*']},
-      zip_safe=False)
+setup(
+    name="chembl_structure_pipeline",
+    version="1.0.0",
+    description="ChEMBL Structure Pipeline",
+    url="https://www.ebi.ac.uk/chembl/",
+    author="Greg Landrum",
+    author_email="greg.landrum@t5informatics.com",
+    license="MIT",
+    packages=["chembl_structure_pipeline"],
+    package_data={"chembl_structure_pipeline": ["data/*"]},
+    zip_safe=False,
+)
