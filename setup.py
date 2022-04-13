5d6c555a4e1ee314a25c1c7b8509f7cf38172038
Dan Mason <dan.mason@healx.io>
2022-04-13 20:40:23 +0100

    remove version frmo setup.py, add it to setup.cfg, and pin min version of setuptools that is compatible

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
     name="chembl_structure_pipeline",
-    version="1.0.0",
     description="ChEMBL Structure Pipeline",
     url="https://www.ebi.ac.uk/chembl/",
     author="Greg Landrum",
@@ -11,6 +10,7 @@ setup(
     packages=["chembl_structure_pipeline"],
     package_data={"chembl_structure_pipeline": ["data/*"]},
     zip_safe=False,
+    install_requires=["setuptools>=46.4.0"],
     extras_require={
         "rdkit": ["rdkit-pypi>=2019.03.1,<=2021.3.5.1"],
     },
