50d89844ca8a5ab6fcf05ed31bcc2c2df844105e
Eloy Félix <eloyfelix@gmail.com>
2022-11-04 15:23:17 +0000

    use RDKit 2022.09

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup
-import chembl_structure_pipeline
 
 setup(
     name="chembl_structure_pipeline",
-    version=chembl_structure_pipeline.__version__,
     description="ChEMBL Structure Pipeline",
     url="https://www.ebi.ac.uk/chembl/",
     author="Greg Landrum",
@@ -11,6 +9,6 @@ setup(
     license="MIT",
     packages=["chembl_structure_pipeline"],
     package_data={"chembl_structure_pipeline": ["data/*"]},
-    install_requires=['rdkit>=2022.09.01'],
+    install_requires=["setuptools>=46.4.0", "rdkit>=2022.09.01"],
     zip_safe=False,
 )
