9b14b92f52c0c055cd6fdc89853dbb447230cffd
Eloy Félix <eloyfelix@gmail.com>
2022-11-04 15:08:04 +0000

    use RDKit 2022.09.01, make it pypi installable

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 import chembl_structure_pipeline
 
-setup(name='chembl_structure_pipeline',
-      version=chembl_structure_pipeline.__version__,
-      description='ChEMBL Structure Pipeline',
-      url='https://www.ebi.ac.uk/chembl/',
-      author='Greg Landrum',
-      author_email='greg.landrum@t5informatics.com',
-      license='MIT',
-      packages=['chembl_structure_pipeline'],
-      package_data={'chembl_structure_pipeline': ['data/*']},
-      zip_safe=False)
+setup(
+    name="chembl_structure_pipeline",
+    version=chembl_structure_pipeline.__version__,
+    description="ChEMBL Structure Pipeline",
+    url="https://www.ebi.ac.uk/chembl/",
+    author="Greg Landrum",
+    author_email="greg.landrum@t5informatics.com",
+    license="MIT",
+    packages=["chembl_structure_pipeline"],
+    package_data={"chembl_structure_pipeline": ["data/*"]},
+    install_requires=['rdkit==2022.09.01'],
+    zip_safe=False,
+)
